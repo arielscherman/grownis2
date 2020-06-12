ac0f3056d5de54989f1d99daeae885b7c408ca77
Ariel Scherman <arielscherman@gmail.com>
2020-06-12 17:39:04 -0300

    add empty message for depots

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 Currency.create([
   { name: "Dolar", symbol: "USD" },
-  { name: "Peso Argentino", symbol: "ARS" },
-  { name: "Bitcoin", symbol: "BTC" }
+  { name: "Peso Argentino", symbol: "ARS" }
 ])
