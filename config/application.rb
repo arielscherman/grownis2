54f2b1e2c441797d84e6e4706b79069e6726a55f
Ariel Scherman <arielscherman@gmail.com>
2020-06-17 09:38:50 -0300

    fetch rates for selected currency

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Grownis2
 
     # Don't generate system test files.
     config.i18n.default_locale = :es
+    config.time_zone = "America/Argentina/Buenos_Aires"
 
     config.active_job.queue_adapter = :sidekiq
   end
