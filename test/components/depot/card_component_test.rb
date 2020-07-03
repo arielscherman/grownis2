04c932212596ad42f5e33bc5c0aa6ef8aecf8262
Ariel Scherman <arielscherman@gmail.com>
2020-07-03 11:57:29 -0300

    add generated date for dasbhoard and more test coverage

--- test/components/depot/card_component_test.rb
+++ test/components/depot/card_component_test.rb
@@ -1,10 +1,21 @@
 require "test_helper"
 
 class Depot::CardComponentTest < ViewComponent::TestCase
-  test "component renders something useful" do
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(Depot::CardComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  include Rails.application.routes.url_helpers
+
+  test "renders links to depot actions" do
+    depot = depots(:national_bank)
+    render_inline(Depot::CardComponent.new(depot: depot))
+
+    assert_selector "a[href='#{depot_path(depot)}']", text: "Ver"
+    assert_selector "a[href='#{depot_movements_path(depot)}']", text: "Movimientos"
+    assert_selector "a[href='#{edit_depot_path(depot)}']", text: "Editar"
+  end
+
+  test "renders depot's name in uppercase" do
+    depot = depots(:national_bank)
+    render_inline(Depot::CardComponent.new(depot: depot))
+
+    assert_selector ".ui-card-title", text: depot.name.upcase
   end
 end
