89dc24a47ffcdd529c147a810978bbddd0f191d1
Ariel Scherman <arielscherman@gmail.com>
2020-07-09 15:57:31 -0300

    7 fix first create of movements which didn't remove empty message

--- test/system/depots/movements/create_test.rb
+++ test/system/depots/movements/create_test.rb
@@ -20,6 +20,31 @@ class Depots::MovementsCreateTest < ApplicationSystemTestCase
     assert_selector ".movement", text: "Personal note"
   end
 
+  test "creating the first movement removes the empty message" do
+    user = users(:valid_with_fresh_depot)
+    depot = user.depots.first
+
+    sign_in user
+
+    visit depot_movements_url(depot_id: depot.id)
+
+    assert_selector ".no-movements"
+
+    click_on "Agregar Movimiento"
+
+    fill_in "Total", with: "300.00"
+    fill_in "Descripción", with: "Personal note"
+
+    find("#depot_movement_date").click
+    find(".flatpickr-day.today").click
+
+    click_on "Guardar"
+
+    assert_no_selector ".no-movements"
+    assert_selector ".movement", text: amount(300_00)
+    assert_selector ".movement", text: "Personal note"
+  end
+
   test "creating a movement causes the balance to update" do
     depot = depots(:national_bank)
 
