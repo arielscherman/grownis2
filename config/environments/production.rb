5990fbfe7f7332218865d9c3376ba6039dfb0311
Ariel Scherman <arielscherman@gmail.com>
2020-07-26 11:16:28 -0300

    add logdna

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.logger = Logdna::Ruby.new(ENV["LOGDNA_KEY"], options)
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
