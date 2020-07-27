6e570fb20c0488cae303aa00fd6d7ceb5cf6f441
Ariel Scherman <arielscherman@gmail.com>
2020-07-27 12:41:32 -0300

    add tests for bug mailer

--- test/mailers/bug_mailer_test.rb
+++ test/mailers/bug_mailer_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BugMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "send email to support" do
+    bug = bugs(:bug)
+    mail = BugMailer.with(bug: bug).bug_email
+
+    assert_includes mail.to, "arielscherman@gmail.com"
+    assert_includes mail.body.encoded, bug.description
+  end
 end
