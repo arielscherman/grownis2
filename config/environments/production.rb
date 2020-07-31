9f6fbd31b5362bdca3984b6c07eeb10a35c376a6
Ariel Scherman <arielscherman@gmail.com>
2020-07-30 21:56:23 -0300

    update mailgun setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'grownis.com',
+    :domain         => ENV['MAILGUN_DOMAIN'],
     :authentication => :plain,
   }
   config.action_mailer.default_url_options = { :host => 'grownis.com' }
