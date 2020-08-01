5ebd01d68f038666d7b0ea880e430117dc79dbaa
Ariel Scherman <arielscherman@gmail.com>
2020-08-01 10:36:06 -0300

    add sendgrid setup instead of mailgun

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = '5b027f5ba921400dbed3b03840397acb'
+  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   # Here we'll disable in 'test':
   if Rails.env.test? || Rails.env.development?
