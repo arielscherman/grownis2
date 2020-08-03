1744763423a4a0664485be32119e9125ee67c630
Ariel Scherman <arielscherman@gmail.com>
2020-08-03 12:33:38 -0300

    test sendgrid fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,5 +119,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { :host => 'grownis.com' }
+  config.action_mailer.default_url_options = { :host => 'https://grownis.com' }
 end
