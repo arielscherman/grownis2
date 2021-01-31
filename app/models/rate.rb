b44627e1830946d4c5a491d96fd506a03515d638
Ariel Scherman <arielscherman@gmail.com>
2021-01-31 18:28:30 -0300

    set latest as optional

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,7 +1,7 @@
 class Rate < ApplicationRecord
   belongs_to :currency
   belongs_to :to_currency, class_name: "Currency"
-  belongs_to :latest_value, class_name: "Rate::Value"
+  belongs_to :latest_value, class_name: "Rate::Value", optional: true
   has_many :values, class_name: "Rate::Value", inverse_of: :rate, dependent: :destroy
   has_many :depots, inverse_of: :rate, dependent: :restrict_with_exception
 
